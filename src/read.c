b835f989547a5e43d5f933c3d180440940d77a4b
sneisius <sneisius@uci.edu>
2013-04-17 15:01:36 -0700

    trying to improve read performance

--- src/read.c
+++ src/read.c
@@ -1112,9 +1112,9 @@ read_a_source_file (char *name)
 		      break;
 		    }
 
-		  new_buf = (char *) xrealloc (new_buf, new_length + 10000);//EDIT
+		  new_buf = (char *) xrealloc (new_buf, new_length + 100);
 		  new_tmp = new_buf + new_length;
-		  new_length += 10000;//EDIT
+		  new_length += 100;
 		}
 
 	      if (tmp_buf)
